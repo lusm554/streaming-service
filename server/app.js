d299f0ad57a9a6840ee80ef8fec485d4158dfcf3
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-31 19:53:42 +0300

    test

--- server/app.js
+++ server/app.js
@@ -1,1 +1,9 @@
-/* server here */
+const app = require('express')();
+const config = require('config')
+const bodyParser = require('body-parser')
+
+app.get('/', (req, res) => {
+    res.send('Hi')
+})
+
+app.listen(config.get('port'))

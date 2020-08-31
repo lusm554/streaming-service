51773557e87b88bcabbd949935ce5cd73f136498
loveyousomuch <loveyousomuch554@gmail.com>
2020-08-31 22:31:23 +0300

    test stream

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.clickHandler = this.clickHandler.bind(this)
+  }
+
+  clickHandler() {
+    startCapture()
+  }
+
   render() {
     return (
-      <h1>Hello World!</h1>
+      <div>
+        <input type="button" onClick={this.clickHandler} value="Start stream"></input>
+      </div>
     )
   }
+}
+
+async function startCapture(displayMediaOptions) {
+  let captureStream = null;
+
+  try {
+    captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+  } catch(err) {
+    console.error("Error: " + err);
+  }
+  return captureStream;
 }
 
 export default App;
--- server/app.js
+++ server/app.js
@@ -1,9 +1,21 @@
-const app = require('express')();
+const express = require('express');
+const app = express()
 const config = require('config')
 const bodyParser = require('body-parser')
+const path = require('path')
+require('dotenv').config()
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')))
+}
 
 app.get('/', (req, res) => {
-    res.send('Hi')
+    res.send('1')
 })
 
 app.listen(config.get('port'))

5471a7e3910c7a4e1a9fcd975b91e89a04cf24e9
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-01 20:54:40 +0300

    components

--- client/src/App.js
+++ client/src/App.js
@@ -1,53 +1,22 @@
 import React, { Component } from 'react';
+import Stream from './components/Stream'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams
+} from "react-router-dom";
 
 class App extends Component {
-  constructor() {
-    super()
-    this.clickHandler = this.clickHandler.bind(this)
-  }
-
-  async clickHandler() {
-    // options to access the screen with sound on
-    const gdmOptions = {
-      video: {
-        cursor: "always"
-      },
-      audio: {
-        echoCancellation: true,
-        noiseSuppression: true,
-        sampleRate: 44100
-      }
-    }
-
-    let mediaStream = await startCapture(gdmOptions)
-    console.log(mediaStream.id)
-  }
-
   render() {
     return (
       <div>
-        <input type="button" onClick={this.clickHandler} value="Start stream"></input>
+        <Stream />
       </div>
     )
   }
-}
-
-async function startCapture(displayMediaOptions) {
-  let captureStream = null
-
-  try {
-    captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
-  } catch(err) {
-    const isBrowserCannotStream = err.stack && err.stack.includes('getDisplayMedia') 
-
-    if(isBrowserCannotStream) {
-      alert('You cannot stream with this browser')
-    }
-    else {
-      console.log(err)
-    }
-  }
-  return captureStream
 }
 
 export default App
---
+++ client/src/components/Stream.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+
+class Stream extends Component {
+    constructor() {
+        super()
+        this.clickHandler = this.clickHandler.bind(this)
+  }
+
+    async clickHandler() {
+        // options to access the screen with sound on
+        const gdmOptions = {
+        video: {
+            cursor: "always"
+        },
+        audio: {
+            echoCancellation: true,
+            noiseSuppression: true,
+            sampleRate: 44100
+        }
+        }
+
+        let mediaStream = await startCapture(gdmOptions)
+        console.log(mediaStream)
+    }
+
+    render() {
+        return (
+        <div>
+            <input type="button" onClick={this.clickHandler} value="Start stream"></input>
+        </div>
+        )
+    }
+}
+
+async function startCapture(displayMediaOptions) {
+    let captureStream = null
+  
+    try {
+      captureStream = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions)
+    } catch(err) {
+      const isBrowserCannotStream = err.stack && err.stack.includes('getDisplayMedia') 
+  
+      if(isBrowserCannotStream) {
+        alert('You cannot stream with this browser')
+      }
+      else {
+        console.log(err)
+      }
+    }
+    return captureStream
+}
+
+export default Stream

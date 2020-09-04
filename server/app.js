9f0f0e0a7ad952f73585ac394ecf85be10248506
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-04 16:04:53 +0300

    cleared the authorization

--- server/app.js
+++ server/app.js
@@ -5,12 +5,6 @@ const bodyParser = require('body-parser')
 const path = require('path')
 const mongoose = require('mongoose')
 require('dotenv').config()
-const passport = require('passport')
-const User = require('./models/user')
-const cookieParser = require('cookie-parser')
-const session = require('express-session')
-const cookieSession = require('cookie-session')
-const cors = require('cors')
 
 mongoose.connect(config.get('mongoID'), { useNewUrlParser: true, useUnifiedTopology: true })
 
@@ -18,9 +12,6 @@ mongoose.connect(config.get('mongoID'), { useNewUrlParser: true, useUnifiedTopol
 app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
-
-
-// app.use('/auth', require('./routes/auth-routes'))
 
 if(process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '..', 'client', 'build')))

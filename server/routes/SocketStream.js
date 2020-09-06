3cb442dbff20d3351e7e3db631b7f23fbb913c66
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-06 23:54:07 +0300

    ws

--- server/routes/SocketStream.js
+++ server/routes/SocketStream.js
@@ -1,2 +1,11 @@
-const ws = require('ws')
+const WebSocket = require('ws')
 const server = require('../app')
+const wss = new WebSocket.Server({ server })
+
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+      console.log('received: %s', message);
+    });
+   
+    ws.send('something');
+});

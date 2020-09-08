3398b1d51d8094b572f41de1fc4000a3f96faee2
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-08 20:39:24 +0300

    send to client

--- server/routes/SocketStream.js
+++ server/routes/SocketStream.js
@@ -4,14 +4,14 @@ const wss = new WebSocket.Server({ server })
 
 wss.on('connection', function connection(ws) {
     ws.on('message', function incoming(message) {
-        // let clientObj = JSON.parse(message)
+        let clientObj = JSON.parse(message)
         console.log(message)
+        sendToClient(ws, message)
     });
    
-    ws.send(
-        JSON.stringify({
-            type: 'message',
-            data: 'hi'
-        })
-    );
 });
+
+function sendToClient(ws, msg) {
+    let msgJSON = JSON.stringify(msg)
+    ws.send(msgJSON)
+}

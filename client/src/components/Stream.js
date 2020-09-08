64ba6c8b323277e87b13ae2bf27427723ef48719
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-08 14:40:58 +0300

    remove socket

--- client/src/components/Stream.js
+++ client/src/components/Stream.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 
-const ws = new WebSocket('ws://localhost:8080')
+// const ws = new WebSocket('ws://localhost:8080')
 
-ws.addEventListener('open', (e) => {
-    console.log(e)
-})
+
+// ws.addEventListener('open', (e) => {
+//     console.log(e)
+// })
 
 class Stream extends Component {
     constructor(props) {

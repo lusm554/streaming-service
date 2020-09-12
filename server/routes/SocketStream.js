ddf04feaab1cd57c345160f0818eb90ee8d35541
loveyousomuch <loveyousomuch554@gmail.com>
2020-09-12 15:28:35 +0300

    processing different types of messages and sending a stream to all connected users

--- server/routes/SocketStream.js
+++ server/routes/SocketStream.js
@@ -6,19 +6,36 @@ wss.on('connection', function connection(ws) {
     // Setting up a user for the websocket.
     ws.user = server.user
 
-    ws.on('message', function incoming(message) {
-        let clientObj = JSON.parse(message)
-        console.log(message)
-        sendToClient(ws, clientObj)
+    ws.on('message', function incoming(json_message) {
+        let msg = JSON.parse(json_message)
+
+        switch (msg.type) {
+            case 'video-offer':
+            case 'new-ice-candidate':
+                sendStream(ws, json_message)
+                break;
+            case 'message': 
+                sendStream(ws, json_message);
+                break;
+            default:
+                break;
+        }
     });
    
 });
 
-function sendToClient(ws, msg) {
+function echo(ws, msg) {
     let msgJSON = JSON.stringify(msg)
     ws.send(msgJSON)
 }
 
-function sendStream(msg) {
+function sendStream(ws, msg) {
     let clients = wss.clients
+
+    for(let client of clients) {
+        let { id } = client.user
+        if(ws.user.id != id) {
+            client.send(msg)
+        }
+    }
 }
